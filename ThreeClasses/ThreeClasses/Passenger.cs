517acb585a273abf29200fc3a01fccdedac9d901
CJF86 <77642189+CJF86@users.noreply.github.com>
2021-02-03 05:01:15 -0500

    FirstCheckIn
    Created three separate classes and defined public variables in said classes

---
+++ ThreeClasses/ThreeClasses/Passenger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeClasses
+{
+    class Passenger
+    {
+        public string FirstName; //creates public string FirstName
+        public string LastName; //creates public string LastName
+
+        public Passenger(string First_Name = null, string Last_Name = null) //creates method with First_Name and Last_Name as parameters
+        {
+            FirstName = First_Name;
+            LastName = Last_Name;
+            Console.WriteLine("Customer" + First_Name + Last_Name); //writes customers first and last name to the console
+
+        }
+    }
+}
---
+++ ThreeClasses/ThreeClasses/Ticket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeClasses
+{
+    class Ticket : Passenger
+    {
+        public int NumberOfTickets;
+        public string TypeOfTicket;
+    }
+}
