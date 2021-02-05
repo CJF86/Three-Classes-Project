1fb9d81abdc33669707eeb4445f2018c89b79522
CJF86 <77642189+CJF86@users.noreply.github.com>
2021-02-04 20:31:09 -0500

    Updated classes

--- ThreeClasses/ThreeClasses/Ticket.cs
+++ ThreeClasses/ThreeClasses/Ticket.cs
@@ -8,7 +8,59 @@ namespace ThreeClasses
 {
     class Ticket : Passenger
     {
-        public int NumberOfTickets;
-        public string TypeOfTicket;
+        public int NumberOfTickets; //create constructor for variable
+        public string TypeOfTicket; //create constructor for variable
+        public bool Switch;
+        
+
+        public Ticket(int NumberOfTickets,string TypeOfTicket)
+        {
+            Console.WriteLine("How many tickets do you want: ");
+            NumberOfTickets = int.Parse(Console.ReadLine());
+
+            while (NumberOfTickets < 1)
+            {
+                if (NumberOfTickets == 0)
+                {
+                    Console.WriteLine("Please enter a higher number: ");
+                    int v = int.Parse(Console.ReadLine());
+                    NumberOfTickets = v;
+
+                }
+                else
+                {
+                    Console.WriteLine("You want " + NumberOfTickets + "tickets");
+                    break;
+
+                }
+            }
+
+            Console.WriteLine("What type of ticket do you want");
+            TypeOfTicket = Console.ReadLine();
+
+            while (Switch == false)
+            {
+                if (TypeOfTicket != "first class" || TypeOfTicket != "business")
+                {
+                    Console.WriteLine("Please enter a ticket type: ");
+                    TypeOfTicket = Console.ReadLine();
+                    Switch = false;
+                }
+                else if (TypeOfTicket == "first class")
+                {
+                    Console.WriteLine("You have selected first class");
+                    Switch = true;
+                }
+                else if (TypeOfTicket == "business")
+                {
+                    Console.WriteLine("You have selected business");
+                    Switch = true;
+                }
+            }
+
+            
+
+
+        }
     }
 }
--- ThreeClasses/ThreeClasses/Train.cs
+++ ThreeClasses/ThreeClasses/Train.cs
@@ -8,6 +8,38 @@ namespace ThreeClasses
 {
     class Train
     {
-        public string TypeOfTrain;
+        public string TypeOfTrain;//Do not know why it doesn't like this but it doesn't
+        public string User_Input;
+        public bool LoopBreak;//boolean variable used to break out of loops
+
+        public Train (string TypeOfTrain)
+        {
+            Console.WriteLine("What type of train do you want: ");
+            User_Input = Console.ReadLine();
+            TypeOfTrain = User_Input; //potentially an error
+            LoopBreak = false;
+
+            while(LoopBreak == false)
+            {
+                if (TypeOfTrain != "electric" || TypeOfTrain != "coal")
+                {
+                    Console.WriteLine("Please select a type of train: ");
+                    User_Input = Console.ReadLine();
+                    TypeOfTrain = User_Input;
+                    LoopBreak = false;
+
+                }
+                else if (TypeOfTrain == "electric")
+                {
+                    Console.WriteLine("You have chosen an electric train");
+                    LoopBreak = true;
+                }
+                else if (TypeOfTrain == "coal")
+                {
+                    Console.WriteLine("You have chosen a coal powered train");
+                    LoopBreak = true;
+                }
+            }
+        }
     }
 }
